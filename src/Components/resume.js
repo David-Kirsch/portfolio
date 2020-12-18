d07e081313f94c12249fae79af1a98ad39b2fb6e
David-Kirsch <davidjkirsch@gmail.com>
2020-12-18 16:22:18 -0500

    Removed Baloo font, changed profile pic measurement, adjusted banner text

--- src/Components/resume.js
+++ src/Components/resume.js
@@ -5,13 +5,14 @@ import DavidDesk from '../images/desk.JPG'
 import Education from './education'
 import ResumeDoc from '../images/David_Kirsch_Resume.pdf'
 
+
 class Resume extends Component {
     render() {
         return (
             <div className="resume" id={'resume'}>
                 <h1>Resume</h1>
                     <div className='resume-container'>
-                        <embed src={`${ResumeDoc}#toolbar=1&scrollbar=0`} type="application/pdf" height="1090" width="800"></embed>
+                        <embed src={`${ResumeDoc}#toolbar=1&scrollbar=0`} type="application/pdf"></embed>
                     </div>
             </div>
         )
