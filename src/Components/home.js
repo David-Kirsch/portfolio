25e08df28617997c85d2dcd84f29f300cfabdb61
David Kirsch <39254033+David-Kirsch@users.noreply.github.com>
2022-07-04 17:28:04 -0400

    Update home.js

--- src/Components/home.js
+++ src/Components/home.js
@@ -14,7 +14,7 @@ class LandingPage extends Component {
                         />
                         <div className='banner-text'>
                             <h1 className='name'>David Kirsch</h1>
-                            <h1>Android &amp; Web Developer</h1>
+                            <h1>Software Engineer &lpar;Android &amp; Web&rpar;</h1>
                             <hr/>
                             <p>Kotlin | React | JavaScript | Ruby | Ruby on Rails | Java | HTML/CSS | SQLite | PostgreSQL</p>
                             <div className="social-links">
